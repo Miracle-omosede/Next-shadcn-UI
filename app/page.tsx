4e6f5505e0e9e44136b63acc230a53d70a26e617
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-09-22 11:40:06 +0100

    header file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="grid place-items-center h-screen">
-     <Button size={'lg'} variant={'secondary'}>Click Me</Button>
-    </main>
+    <div>
+      <Header/>
+    </div>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='sm:flex sm:justify-between py-3 px-4 border-b'>
+    </header>
+  )
+}
+
+export default Header
