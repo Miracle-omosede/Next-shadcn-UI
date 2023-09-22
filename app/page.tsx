3138d15f7c2f9d66a4dae524e77e5ceb7caba0c0
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-09-22 11:29:44 +0100

    shadcn installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="grid place-items-center h-screen">
+     <Button size={'lg'} variant={'secondary'}>Click Me</Button>
     </main>
   )
 }
