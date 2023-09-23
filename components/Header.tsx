aa7297f2f6063c798dfaca86f4de57056d4a369f
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-09-23 01:51:23 +0100

    header file

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Container from './ui/container'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <header className='sm:flex sm:justify-between py-3 px-4 border-b'>
+        <Container>
+            <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full'>
+                <div className='flex items-center'>
+                    <Link href="/" >
+                        <h1 className='text-xl font-bold uppercase'>
+                            store name
+                        </h1>
+                    </Link>
+                </div>
+            </div>
+        </Container>
     </header>
   )
 }
--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,1 +1,16 @@
-ra
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.
+FC<ContainerProps> = ({
+    children
+}) => {
+    return (
+        <div className="mx-auto w-full max-w-7xl">
+            {children}
+        </div>
+    )
+};
+
+export default Container;
